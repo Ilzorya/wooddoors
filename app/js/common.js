bf19d8918e63f2faaf4f137ceed9dba61118cf66
Ilya Zoria <ilzorya@gmail.com>
2016-09-13 19:01:34 +0300

    v7.7 mobile

--- app/js/common.js
+++ app/js/common.js
@@ -3,6 +3,7 @@ $(function() {
 	$(document).ready(function($){
 
     // accordion
+
     var panels = $('.accordion > dd').hide();
 
     $('.accordion > dt').on('click', function(e) {
@@ -11,34 +12,38 @@ $(function() {
       var $this 			= $(this),
 			    description = $this.next('dd');
 
-      // Slide up all other panels
 			$this.siblings('dt').next().slideUp();
 
 
-      //Slide down target panel
 			if (description.css('display') === 'block') {
 				description.slideUp();
-				$this.find('.arrow').removeClass('arrow-up');
+				$this.find('.arrow').removeClass('arrow_up');
 			} else if (description.css('display') === 'none') {
 				description.slideDown();
-				$this.find('.arrow').addClass('arrow-up');
+				$this.find('.arrow').addClass('arrow_up');
 			}
 
-			$this.siblings().find('.arrow').removeClass('arrow-up');
+			$this.siblings().find('.arrow').removeClass('arrow_up');
 
       return false;
     });
 
+		//mob menu
+
 		$('.mob_menu_button').on('click', function(){
-			$('nav').addClass('menu_show');
+			$('.head_nav').addClass('menu_show');
 			$('.close_icon')
 			.addClass('close_icon_show')
 			.removeClass('close_icon');
+			$('.logo_wrapper').addClass('logo_fixed').removeClass('logo_wrapper');
+
+			$('.close_icon_show').on('click', function(){
+				$(this).removeClass('close_icon_show').addClass('close_icon');
+				$('.head_nav').removeClass('menu_show');
+				$('.logo_fixed').addClass('logo_wrapper').removeClass('logo_fixed');
+			});
 		});
 
-		$('.close_icon_show').on('click', function(){
-			$('nav').removeClass('menu_show');
-		});
 
   });
 });
